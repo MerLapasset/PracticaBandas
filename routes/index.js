dad3306eef56f059d2ff68121305f68b8f7ab6b0
Mercedes Lapasset <mer@MacBook-Air-de-Mercedes.local>
2024-04-06 17:43:26 -0300

    Rutas hechas

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let router = express.Router();
 
-let controlllerGeneral= require("../contr")
+let controllerGeneral= require("../controllers")
 
 /* GET home page. */
-router.get('/:registrado?',controlllerGeneral.index);
-
+router.get('/:registrado?',controllerGeneral.index);
+router.get("/producto/:registrado?/:id",controllerGeneral.producto)
+router.get("/profile/:registrado?/:usuario",controllerGeneral.usuario)
+router.get ("/producto/:registrado?/add/:id",controllerGeneral.addProducto)
+router.get ("/registro/:registrado?",controllerGeneral.registro)
+router.get("/login/:registrado?",controllerGeneral.login)
 
 
 
