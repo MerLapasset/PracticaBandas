6dd4c4b212d5a8f981dfda99b6e25c13f3f17b24
Markelesperanza <mesperanza@udesa.edu.ar>
2024-04-07 17:27:55 -0300

    nuevo controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,19 @@
 const dataBase_info = require('../db/infoData')
 const dataBase = dataBase_info.listaData
 
-const listaController = {}
+const listaController = {
+    index: function(req, res){
+
+        return res.send() //Traer info: Imagen, nobre del producto, descripcion, cantidad comentarios
+    },
+    product: function(req, res){
+
+        return res.send() //Traer info: Imagen, nobre del producto, descripcion, comentario o su nombre de usuario
+    },
+    profile: function(req, res){
+
+        return res.send() //Traer info: Imagen usuario, nobre de usuario, y mis productos (Imagen, nobre del producto, descripcion, cantidad comentarios)
+    },
+}
 
 module.exports = listaController
